d90fa8f39b144ac1f8eb7df3238302e25f958681
crunchengine <crunchengine@gmail.com>
2014-07-05 11:21:27 -0400

    added taxrate and vendorcredit query

--- QuickBooks/IPP/Service/TaxRate.php
+++ QuickBooks/IPP/Service/TaxRate.php
@@ -19,14 +19,8 @@
 
 QuickBooks_Loader::load('/QuickBooks/IPP/Service.php');
 
-class QuickBooks_IPP_Service_VendorCredit extends QuickBooks_IPP_Service
+class QuickBooks_IPP_Service_TaxRate extends QuickBooks_IPP_Service
 {
-	public function findAll($Context, $realmID)
-	{
-		$xml = null;
-		return parent::_findAll($Context, $realmID, QuickBooks_IPP_IDS::RESOURCE_VENDORCREDIT, $xml);
-	}
-
 	public function query($Context, $realm, $query)
 	{
 		return parent::_query($Context, $realm, $query);
